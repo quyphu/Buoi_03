6910aa905117d5b33aab8d615028862e8914de82
Nguyen Ngoc Phu Quy <quyphu059a@gmail.com>
2024-07-24 10:22:07 +0700

    Hinh Anh DeBug_QuickSort va Tim Kiem Nhi Phan Khong De Quy

---
+++ Buoi_03/TKNP_KhongDeQui/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 5, 7, 23, 32, 34, 62 };
+        int target = 23;
+        int result = BinarySearchNonRecursive(array, target);
+        Console.WriteLine("Vi Tri Phan Tu " + target + " (Khong Co De Qui): " + result);
+        Console.ReadKey();
+    }
+
+    static int BinarySearchNonRecursive(int[] array, int target)
+    {
+        int left = 0, right = array.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target)
+                return mid;
+            if (array[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return -1; // Không tìm thấy
+    }
+}
