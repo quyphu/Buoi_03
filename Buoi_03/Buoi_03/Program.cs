cc6ca521fa87f550a49e5c3673da116682b95a9c
Nguyen Ngoc Phu Quy <quyphu059a@gmail.com>
2024-07-24 09:58:42 +0700

    Tinh Tong Mang Khong De Quy

---
+++ Buoi_03/Buoi_03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int sum = SumArrayNonRecursive(array);
+        Console.WriteLine("Tổng : " + sum);
+        Console.ReadKey();
+    }
+
+    static int SumArrayNonRecursive(int[] array)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            sum += array[i];
+        }
+        return sum;
+        
+    }
+ 
+    
+}
