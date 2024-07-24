ccfb865c384dab0b575456cc50728bf354eaa090
Nguyen Ngoc Phu Quy <quyphu059a@gmail.com>
2024-07-24 10:28:23 +0700

    Tim Kiem Nhi Phan De Quy

---
+++ Buoi_03/TKNP_DeQui/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 5, 7, 23, 32, 34, 62 };
+        int target = 23;
+        int result = BinarySearchRecursive(array, target, 0, array.Length - 1);
+        Console.WriteLine("Vi tri cua phan tu " + target + " (de quy): " + result);
+        Console.ReadKey();
+    }
+
+    static int BinarySearchRecursive(int[] array, int target, int left, int right)
+    {
+        if (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target)
+                return mid;
+            if (array[mid] < target)
+                return BinarySearchRecursive(array, target, mid + 1, right);
+            else
+                return BinarySearchRecursive(array, target, left, mid - 1);
+        }
+
+        return -1;
+    }
+}
